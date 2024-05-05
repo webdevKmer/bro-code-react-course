06354bfef9cfd43d1bee9bd6cdb1792fb8357ef1
webdevKmer <webdevKmer@gmail.com>
2024-05-05 19:30:12 +0000

    fix main.tsx and App.tsx

---
+++ react-apps/src/App.tsx
@@ -0,0 +1,17 @@
+import BasicsReact from "./BasicsReact"
+import ColorPicker from "./ColorPicker"
+import Counter from "./Counter"
+
+function App() {
+  return (
+    <>
+      <ColorPicker />
+      <hr />
+      <BasicsReact />
+      <hr />
+      <Counter/>
+    </>
+  )
+}
+
+export default App
